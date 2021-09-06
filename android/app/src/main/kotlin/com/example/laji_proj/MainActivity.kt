a6605251d08898c07e4be0dc2a32a3a2ed3f3d08
7axiomsOfEuclid <axiom88guru@gmail.com>
2021-09-06 22:47:10 +0900

    Revert "init"
    This reverts commit 49a033320ecace16396c7527b25476ebe3e31a77.

--- android/app/src/main/kotlin/com/example/laji_proj/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.laji_proj
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/const_config.dart
+++
@@ -1,7 +0,0 @@
-import 'package:amap_flutter_base/amap_flutter_base.dart';
-
-class ConstConfig {
-  static const AMapApiKey amapApiKeys = AMapApiKey(
-      androidKey: 'e3c2a195f85d7d278e1aa7658cbf7864',
-      iosKey: '4dfdec97b7bf0b8c13e94777103015a9');
-}
